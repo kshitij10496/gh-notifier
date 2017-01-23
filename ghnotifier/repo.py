7d77eeba813e4a3d5f6756f042c3c25fd846606d
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-01-23 13:26:18 +0530

    modified repo class

--- ghnotifier/repo.py
+++ ghnotifier/repo.py
@@ -1,21 +1,43 @@
 class Repo(object):
 
-    def __init__(self, name):
-        self._name = name
-        self._forks = None
+    def __init__(self, name, owner, forks, watchers, stars, url):
+        """
+        Parameters
+        ==========
+        name: str
+            Name of the repository
+
+        owner: User
+            Owner of the repository
+
+        forks: int
+            Forks count of the repository
+
+        watchers: int
+            Watchers count of the repository
+
+        stars: int
+            Stars count of the repository
+
+        url: str
+            API URL of the repository
+        """
+        self.name = name
+        self.owner = owner
+        self.forks = forks
+        self.watchers = watchers
+        self.stars = stars
+        self.url = url
 
     def __repr__(self):
-        return '{}({})'.format(self.__class__.__name__, self._name)
+        return '{}({})'.format(self.__class__.__name__, self.name, self.owner,
+                               self.forks, self.watchers, self.stars)
 
-    @property
-    def forks(self):
-        self._forks = 10
-
-    @property
-    def owner(self):
-
-    @property
-    def watchers(self):
-
-    @property
-    def stargazers(self):
+    @classmethod
+    def from_name(cls, name):
+        owner_name = get_user()
+        owner = User(owner_name)
+        forks = get_forks()
+        watchers = get_watchers()
+        stars = get_stars()
+        return cls(name, owner, forks, watchers, stars)
