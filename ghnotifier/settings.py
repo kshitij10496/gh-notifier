a98a307476a617fd2b93feeb85517e486c1c1383
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-01-24 22:24:35 +0530

    storing the notifier and platform

--- ghnotifier/settings.py
+++ ghnotifier/settings.py
@@ -5,6 +5,19 @@ USER_CREDENTIALS = os.path.expanduser('~') + '/.ghnotifier_config.json'
 USER_DATA = os.path.expanduser('~') + '/.ghnotifier_data.json'
 
 PLATFORM = sys.platform
+NOTIFIER = None
+if PLATFORM == 'darwin':
+    # test if terminal-notifier is present on the system or not
+    status = subprocess.run(['which', 'terminal-notifier']).returncode
+    if status == 0:
+        NOTIFIER = "terminal-notifier"
+    else:
+        print("Kindly install terminal-notifier for MacOS.") # Raise exception
+elif PLATFORM == 'linux':
+    NOTIFIER = "notify-send"
 
+else:
+    print("Your system is not supported yet.") # Raise exception
+        
 URL = 'https://api.github.com' 
 HEADERS = {'Accept': 'application/vnd.github.v3+json'}
