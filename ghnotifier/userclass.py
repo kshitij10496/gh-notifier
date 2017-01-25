cedb212cabce7e6076c6a93641b816d8bfc1bae9
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-01-25 20:48:42 +0530

    added method for serialising

--- ghnotifier/userclass.py
+++ ghnotifier/userclass.py
@@ -1,6 +1,13 @@
+import json
+import requests
+
+from notification import Notification
+from repo import Repo
+from settings import USER_DATA, USERNAME, PASSWORD, URL, HEADERS
+
 class User(object):
 
-    def __init__(self, username, followers, repos):
+    def __init__(self, username, followers=0, repos=None):
         """ Represents a user encapsulating the GitHub User object.
 
           Parameters
@@ -22,48 +29,25 @@ class User(object):
         self.followers = followers
         self.repos = repos
  
-    def __repr__():
-        return "{}({})".format(self.__class__.__name__, self.username)
+    def __repr__(self):
+        return "{}({}, {}, {})".format(self.__class__.__name__, self.username,
+                                       self.followers, self.repos)
 
     @classmethod
     def from_handle(cls, handle):
         username = handle
-        user = self.get_blob(handle)
+        user = get_blob(handle)
         followers = user['followers']
-        repos = self.get_repos(handle)
+        repos = get_repos(handle)
         return cls(username, followers, repos)
 
-    def get_blob(self, handle):
-        if handle == USERNAME:
-            user_url = URL + '/user'
-
-        else:
-            user_url = URL + '/users/' + handle
-
-        user = requests.get(user_url, headers=HEADERS, auth=(USERNAME, PASSWORD)).json()
-        return user
-
-    def get_repos(self, handle):
-        if self.username == USERNAME:
-            repo_url = URL + '/user/repos'
-
-        else:
-            repo_url = URL + '/users/' + handle + '/repos'
-        
-        repo_response = requests.get(repo_url, headers=HEADERS, auth=(USERNAME, PASSWORD)).json()
-        repos = [Repo.from_name(repo[name], repo[owner][login]) for repo in repo_response]
-        return repos
-
-    def get_notifications(self):
+    def get_notifications(self, old_count):
         # changes in followers
         # notification
-        old_count # from DB
         new_count = self.followers
+        notifications = []
 
-        if old_count == new_count:
-            return None
-
-        else:
+        if old_count != new_count:
             if self.username == USERNAME:
                 notification_url = URL + '/user/followers'
                 target = 'you'
@@ -72,25 +56,54 @@ class User(object):
                 notification_url = URL + '/users/' + self.username + '/followers'
                 target = self.username
 
-            notifications = []
             if new_count > old_count:
                 params = {'per_page': self.followers}
                 start_page = old_count // 100
                 end_page = new_count // 100
                 followers = []
-                for i in range(start_page, end_page + 1):
-                    params['page'] = i
-                    followers.append(requests.get(notification_url, headers = HEADERS,
-                                                  params = params, auth=(USERNAME, PASSWORD)))
+                with requests.Session() as s:
+                    for i in range(start_page + 1, end_page + 2):
+                        params['page'] = i
+                        new_page = s.get(notification_url, headers = HEADERS,
+                                            params = params, auth=(USERNAME, PASSWORD)).json()
+                        print("Length of page {} : {}".format(i, len(new_page)))
+                        followers += new_page
 
                 start_index = old_count % 100
                 context = "follow"
+                print("Len of followers:" + str(len(followers)))
+                print("Len of list:" + str(len(followers[start_index:])))
                 for user_blob in followers[start_index:]:
                     protagonist = user_blob['login']
-                    notifications.append(Notification.generate_message(target, protagonist, context))
+                    notifications += [Notification.generate_message(protagonist, context, target)]
 
             else:
                 message = '{} users unfollowed {}'.format(old_count - new_count, target)
-                notifications.append(Notification(message))
+                notifications += [Notification(message)]
 
         return notifications
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+def get_blob(handle):
+    if handle == USERNAME:
+        user_url = URL + '/user'
+
+    else:
+        user_url = URL + '/users/' + handle
+
+    print("User = " + USERNAME + " : " + user_url)
+    user = requests.get(user_url, headers=HEADERS, auth=(USERNAME, PASSWORD)).json()
+    return user
+
+def get_repos(handle):
+    if handle == USERNAME:
+        repo_url = URL + '/user/repos'
+
+    else:
+        repo_url = URL + '/users/' + handle + '/repos'
+    
+    repo_response = requests.get(repo_url, headers=HEADERS, auth=(USERNAME, PASSWORD)).json()
+    repos = [Repo.from_Repository(repo) for repo in repo_response]
+    return repos
