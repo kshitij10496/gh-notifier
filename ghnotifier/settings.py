9becc21b8f96ad66407c98e6f609cbf027a4a36b
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-01-25 20:52:37 +0530

    added files to settings

--- ghnotifier/settings.py
+++ ghnotifier/settings.py
@@ -1,8 +1,29 @@
 import os
 import sys
+import subprocess
+import json
 
 USER_CREDENTIALS = os.path.expanduser('~') + '/.ghnotifier_config.json'
 USER_DATA = os.path.expanduser('~') + '/.ghnotifier_data.json'
+
+if os.path.isfile(USER_CREDENTIALS):
+    with open(USER_CREDENTIALS) as f:
+        CREDENTIALS = json.load(f)
+
+    USERNAME = CREDENTIALS['USERNAME']
+    PASSWORD = CREDENTIALS['PASSWORD']
+
+else:
+    USERNAME = input("Enter your GitHub username: ")
+    PASSWORD = getpass.getpass(prompt="Enter your password: ")
+    CREDENTIALS = {
+                    "USERNAME": USERNAME,
+                    "PASSWORD": PASSWORD 
+                    }
+            
+    with open(USER_CREDENTIALS, 'w') as f:
+        json.dump(CREDENTIALS, f)
+    
 
 PLATFORM = sys.platform
 NOTIFIER = None
