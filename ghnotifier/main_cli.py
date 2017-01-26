4853519724921f93ed25f86693125490fe1c9c87
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-01-26 14:35:06 +0530

    code cleanup

--- ghnotifier/main_cli.py
+++ ghnotifier/main_cli.py
@@ -1,14 +1,11 @@
 import schedule
-import time
 
 from handler import job
 
 def main():
     schedule.every(5).minutes.do(job)
-
     while True:
         schedule.run_pending()
-        time.sleep(1)
 
 if __name__ == "__main__":
     main()
--- ghnotifier/userclass.py
+++ ghnotifier/userclass.py
@@ -6,9 +6,10 @@ from repo import Repo
 from settings import USER_DATA, USERNAME, PASSWORD, URL, HEADERS
 
 class User(object):
+    """ Represents a user encapsulating the GitHub User object. """
 
     def __init__(self, username, followers=0, repos=None):
-        """ Represents a user encapsulating the GitHub User object.
+        """ Initialises a User object.
 
           Parameters
         ==========
@@ -19,7 +20,7 @@ class User(object):
             No. of GitHub followers of the user
 
         repos: list
-            List of all the user's repositories on GitHub. 
+            List of all the user's repositories on GitHub.
             This includes repositories owned by the authenticated user, repositories
             where the user is a collaborator, and repositories that the user has
             access to through an organization membership.
@@ -28,13 +29,21 @@ class User(object):
         self.username = username
         self.followers = followers
         self.repos = repos
- 
+
     def __repr__(self):
         return "{}({}, {}, {})".format(self.__class__.__name__, self.username,
                                        self.followers, self.repos)
 
     @classmethod
     def from_handle(cls, handle):
+        """ Alternate constructor for instantiating an User by its handle.
+
+        Parameters
+        ==========
+        handle: str
+            User's GitHub username/handle.
+
+        """
         username = handle
         user = get_blob(handle)
         followers = user['followers']
@@ -64,8 +73,8 @@ class User(object):
                 with requests.Session() as s:
                     for i in range(start_page + 1, end_page + 2):
                         params['page'] = i
-                        new_page = s.get(notification_url, headers = HEADERS,
-                                            params = params, auth=(USERNAME, PASSWORD)).json()
+                        new_page = s.get(notification_url, headers=HEADERS,
+                                         params=params, auth=(USERNAME, PASSWORD)).json()
                         print("Length of page {} : {}".format(i, len(new_page)))
                         followers += new_page
 
@@ -103,7 +112,7 @@ def get_repos(handle):
 
     else:
         repo_url = URL + '/users/' + handle + '/repos'
-    
+
     repo_response = requests.get(repo_url, headers=HEADERS, auth=(USERNAME, PASSWORD)).json()
     repos = [Repo.from_Repository(repo) for repo in repo_response]
     return repos
